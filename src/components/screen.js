885ecdec28804b86c07b668c37a2cb577fd9fc1a
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2019-10-12 10:29:02 +0200

    Convert screen to functionnal

--- src/components/screen.js
+++ src/components/screen.js
@@ -4,20 +4,19 @@ const screenStyle = width => ({
   width: width * 10 + 10 + 'px'
 });
 
-class Screen extends Component {
-  render() {
-    const table = this.props.data.map((value, index) => (
-      <div
-        key={index}
-        style={{ backgroundColor: `rgb(${value},${value},${value})` }}
-      />
-    ));
-    return (
-      <div className="screenn" style={screenStyle(this.props.width)}>
-        {table}
-      </div>
-    );
-  }
-}
+const Screen = ({ width, height, data = [] }) => {
+  const table = data.map((value, index) => (
+    <div
+      key={index}
+      style={{ backgroundColor: `rgb(${value},${value},${value})` }}
+    />
+  ));
+
+  return (
+    <div className="screenn" style={screenStyle(width)}>
+      {table}
+    </div>
+  );
+};
 
 export default Screen;
